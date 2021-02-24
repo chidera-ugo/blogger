1082fdf0ef534a45a314aab2e36184de77d3df21
chidera-ugo <polarbitllc@gmail.com>
2021-02-24 08:02:06 +0100

    change ig link

--- pages/index.js
+++ pages/index.js
@@ -30,7 +30,7 @@ export default function Home ({ postsData }) {
             </a>
           </span> or {' '}
           <span className="text-blue-600 hover:text-blue-500 inline-block">
-            <a className="flex items-center" href="https://twitter.com/_chideraugo">Instagram
+            <a className="flex items-center" href="https://instagram.com/_chideraugo">Instagram
               <svg className="w-5 ml-1" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
               </svg>
